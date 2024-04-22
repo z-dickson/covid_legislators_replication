fe463ed6de5840e3832f4284fa6c9b8a0a930929
z-dickson <zachdickson94@gmail.com>
2024-04-22 15:54:06 +0100

    update requirements

--- requirements.R
+++ requirements.R
@@ -1,6 +1,10 @@
-# libraries required for the analysis 
 
+# Specify the URL of a CRAN mirror
+cran_mirror <- "https://cran.r-project.org"
+
+# libraries required for the analysis 
+install.packages(install_packages, repos = cran_mirror)
 install_packages <- c("fect", "dplyr", "kableExtra", "modelsummary", "ggplot2", "grf")
 
-# Install packages
-install.packages(install_packages)
+# Install packages from the specified CRAN mirror
+install.packages(install_packages, repos = cran_mirror)
